f093c5612a8ecf81e8da4eaf3d52fd5ccfcef658
Mostafa El-Baramawy <mostafa.elbaramawy@yahoo.com>
2024-08-14 15:25:54 +0300

    populated add button in the details vc controller

--- ToDoListAppWorkShop/DetailsViewController.m
+++ ToDoListAppWorkShop/DetailsViewController.m
@@ -6,8 +6,18 @@
 //
 
 #import "DetailsViewController.h"
+#import "Notes.h"
+#import "ViewController.h"
 
 @interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmantPriority;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmantType;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldDescription;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldTitle;
+@property (nonatomic, strong) Notes *currentNote;
+- (BOOL)isValidTextFields;
+
 
 @end
 
@@ -15,17 +25,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    if (self.currentNote) {
+        self.textFieldTitle.text = self.currentNote.title;
+        self.textFieldDescription.text = self.currentNote.description;
+        self.segmantPriority.selectedSegmentIndex = self.currentNote.priority;
+        self.segmantType.selectedSegmentIndex = self.currentNote.type;
+        self.datePicker.date = self.currentNote.date;
+        
+        // Do any additional setup after loading the view.
+    }
+}
+- (IBAction)addNotes:(id)sender {
+    if ([self isValidTextFields]) {
+        // Update or create new note
+        if (self.currentNote) {
+            self.currentNote.title = self.textFieldTitle.text;
+            self.currentNote.description = self.textFieldDescription.text;
+            self.currentNote.priority = self.segmantPriority.selectedSegmentIndex;
+            self.currentNote.type = self.segmantType.selectedSegmentIndex;
+            self.currentNote.date = self.datePicker.date;
+        } else {
+            self.currentNote = [Notes new];
+            self.textFieldTitle.text = self.currentNote.title;
+            self.textFieldDescription.text = self.currentNote.title;
+            self.segmantPriority.selectedSegmentIndex = self.currentNote.priority;
+            self.segmantType.selectedSegmentIndex = self.currentNote.type;
+            self.datePicker.date = self.currentNote.date;
+            
+        }
+//        // Save to UserDefaults
+//        [self saveNoteToDefaults:self.currentNote];
+//        
+//        // Confirm and dismiss
+//        [self showConfirmationAlert];
+    }
+//    else {
+//        [self showInvalidInputAlert];
+//    }
 }
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (BOOL)isValidTextFields {
+    if (_textFieldTitle.text.length < 1)
+        return FALSE;
+    return TRUE;
 }
-*/
 
 @end
